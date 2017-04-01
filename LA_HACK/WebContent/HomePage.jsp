1f41f091338d696311ba7220a3968484fafc754a
MingzhongGuo <mingzhog@usc.edu>
2017-04-01 11:37:15 -0700

    home page

---
+++ LA_HACK/WebContent/HomePage.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<link rel="stylesheet" type="text/css" href="HomePage.css">
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js">
+	
+	</script>
+<title>Home Page</title>
+</head>
+<body>
+	<div id="top">
+	</div>
+	<div id="bottom">
+	<div id="left">
+	
+	</div>
+	<div id="right">
+	
+	</div>
+	</div>
+	
+</body>
+</html>
--- LA_HACK/src/client/User.java
+++ LA_HACK/src/client/User.java
@@ -1,35 +1,5 @@
 package client;
 
 public class User {
-<<<<<<< HEAD
 
-=======
-	private String username;
-	private String password;
-	private Map<String, String> question;
-	private double money;
-	
-	
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public double getMoney() {
-		return money;
-	}
-	public void setMoney(double money) {
-		this.money = money;
-	}
-	
-	
-	
->>>>>>> cbd627515880b470774cc751d95d526d4c0134eb
 }
